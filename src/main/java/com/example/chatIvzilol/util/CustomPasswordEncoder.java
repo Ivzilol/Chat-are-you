2ee16040dd235589b8ef94d142acf7453de6290d
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-16 18:03:55 +0300

    Initial Commit

---
+++ src/main/java/com/example/chatIvzilol/util/CustomPasswordEncoder.java
@@ -0,0 +1,19 @@
+package com.example.chatIvzilol.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPasswordEncoder {
+
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
