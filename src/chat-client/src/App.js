5af91a5d76e637fe1b22a09e91928ada0f136810
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-18 18:02:59 +0300

    Login user

--- src/chat-client/src/App.js
+++ src/chat-client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Register from "./Components/Register/Register";
 import ConfirmRegister from "./Components/Register/ConfirmRegister";
+import Login from "./Components/Login/Login";
 
 function App() {
     return (
         <Routes>
             <Route path="/register" element={<Register/>}/>
             <Route path="/register/verify/:verificationCode" element={<ConfirmRegister/>}/>
+            <Route path="/login" element={<Login/>}></Route>
         </Routes>
     );
 }
