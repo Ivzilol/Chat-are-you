1be96992b0c17846f3304eb689c175082237df86
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-18 14:52:22 +0300

    Register user

--- src/chat-client/src/Components/Register/Register.js
+++ src/chat-client/src/Components/Register/Register.js
@@ -1,5 +1,49 @@
+import {useUser} from "../../UserProvider/UserProvider";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import baseURL from "../BaseURL/BaseURL";
 
 const Register = () => {
+
+    const user = useUser();
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+
+    useEffect(() => {
+        if (user.jwt) navigate("/")
+    }, [navigate, user.jwt])
+
+    function registerUser() {
+        const requestBody = {
+            username: username,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        }
+        fetch(`${baseURL}api/users/register`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(requestBody),
+        })
+            .then((response) => {
+                if (response.status === 200)
+                    return Promise.all([response.json(), response.headers]);
+                else return Promise.reject("Invalid attempt");
+            })
+            .then(() => {
+                user.setJwt(user.jwt)
+            })
+    }
 
     return (
         <>Register</>
