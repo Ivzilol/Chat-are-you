83b094339c8647b5c000c91bcbb8cda5cadaf121
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-27 09:12:31 +0300

    Save messages

--- src/main/java/com/example/chatIvzilol/controllers/ChatController.java
+++ src/main/java/com/example/chatIvzilol/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.chatIvzilol.controllers;
 
 import com.example.chatIvzilol.model.dto.Message;
+import com.example.chatIvzilol.service.MessageService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -12,12 +13,16 @@ public class ChatController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    public ChatController(SimpMessagingTemplate simpMessagingTemplate) {
+    private final MessageService messageService;
+
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate, MessageService messageService) {
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.messageService = messageService;
     }
 
     @MessageMapping("/message/{room}")
     public void receiveMessage(@Payload Message message, @DestinationVariable String room) {
        simpMessagingTemplate.convertAndSend("/chat-rooms/" + room, message);
+       messageService.saveMessage(message);
     }
 }
--- src/main/java/com/example/chatIvzilol/service/MessageService.java
+++ src/main/java/com/example/chatIvzilol/service/MessageService.java
@@ -1,8 +1,21 @@
 package com.example.chatIvzilol.service;
 
 
+import com.example.chatIvzilol.model.dto.Message;
+import com.example.chatIvzilol.model.entity.ChatMessage;
+import com.example.chatIvzilol.repository.ChatMessageRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
+    private final ChatMessageRepository messageRepository;
+
+    public MessageService(ChatMessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void saveMessage(Message message) {
+        ChatMessage chatMessage = new ChatMessage();
+    }
 }
