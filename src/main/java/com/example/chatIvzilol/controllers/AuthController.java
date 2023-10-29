8c32f942f8bdec59cad4e474fcbe28591180fd10
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-29 16:58:32 +0200

    Synchronized messages.

--- src/main/java/com/example/chatIvzilol/controllers/AuthController.java
+++ src/main/java/com/example/chatIvzilol/controllers/AuthController.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-//@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true", allowedHeaders = "true")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
