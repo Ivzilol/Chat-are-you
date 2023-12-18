5b7547646a8524adc8fc8abba9b562247166f89a
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-18 17:31:27 +0200

    Synchronized save messages.

--- src/main/java/com/example/chatIvzilol/service/ChatRoomService.java
+++ src/main/java/com/example/chatIvzilol/service/ChatRoomService.java
@@ -47,7 +47,6 @@ public class ChatRoomService {
         return false;
     }
 
-
     private static boolean isAdmin(User user) {
         return user.getAuthorities()
                 .stream().anyMatch(auth -> AuthorityEnum.chatModerator.name().equals(auth.getAuthority()));
