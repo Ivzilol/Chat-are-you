ce51f304a7f059c00ddd4f4fd7b16d7ea543afdd
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-28 09:22:43 +0200

    ConstantMessages

--- src/main/java/com/example/chatIvzilol/common/ConstantMessages.java
+++ src/main/java/com/example/chatIvzilol/common/ConstantMessages.java
@@ -8,4 +8,5 @@ public class ConstantMessages {
     public static final String UNSUCCESSFUL_ADD_ROOM = "Unsuccessful add user in room";
     public static final String SUCCESSFUL_CREATE_ROOM = "Successful create chatroom";
     public static final String UNSUCCESSFUL_CREATE_ROOM = "You not create chatroom";
+    public static final String SUCCESSFUL_CHANGE_PASSWORD = "Successful change password";
 }
