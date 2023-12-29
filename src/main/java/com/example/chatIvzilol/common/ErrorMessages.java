39d77ef7ab9667a818065d340bbd920f43b0ea44
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-12-29 14:18:27 +0200

    ConstantMessages

--- src/main/java/com/example/chatIvzilol/common/ErrorMessages.java
+++ src/main/java/com/example/chatIvzilol/common/ErrorMessages.java
@@ -3,4 +3,5 @@ package com.example.chatIvzilol.common;
 public class ErrorMessages {
 
     public static final String INVALID_PASSWORD = "Invalid Password";
+    public static final String INVALID_EMAIL = "Invalid Password";
 }
