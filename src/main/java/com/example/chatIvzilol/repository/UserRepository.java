acfb551a36f3a12bef9f3d9aa2ca96d0a7a4ce0f
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-04 09:52:12 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/repository/UserRepository.java
+++ src/main/java/com/example/chatIvzilol/repository/UserRepository.java
@@ -15,6 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     User findByVerificationCode(String code);
+
     @Query("select u from User as u" +
             " where u.username = :username and u.isValidate = true")
     Optional<User> findByUsernameAndIsValidate(String username);
