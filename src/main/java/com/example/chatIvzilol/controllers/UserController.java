4cab1b318448f209a0bbfe9f22309b7fcc6ad947
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-10 10:35:40 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/controllers/UserController.java
+++ src/main/java/com/example/chatIvzilol/controllers/UserController.java
@@ -80,7 +80,6 @@ public class UserController {
             return ResponseEntity.ok(customResponse);
         }
     }
-
     @PatchMapping("/register/forgotten-password/new-password")
     public ResponseEntity<?> forgottenPasswordNewPassword(@RequestBody ForgottenPasswordNewPasswordDto forgottenPasswordNewPasswordDto) {
         boolean newPassword = this.userService.forgottenPasswordSetNew(forgottenPasswordNewPasswordDto);
