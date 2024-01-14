d283925aa43e711ed0715317d2db7559a4346e5f
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-14 13:27:36 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/util/JwtUtil.java
+++ src/main/java/com/example/chatIvzilol/util/JwtUtil.java
@@ -37,7 +37,6 @@ public class JwtUtil implements Serializable {
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
-
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
