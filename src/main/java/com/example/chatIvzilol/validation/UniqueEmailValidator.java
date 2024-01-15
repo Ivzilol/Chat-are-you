e66b55334fae85d2e4d2a983251c2689f8defdca
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-15 11:32:47 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/validation/UniqueEmailValidator.java
+++ src/main/java/com/example/chatIvzilol/validation/UniqueEmailValidator.java
@@ -10,7 +10,6 @@ public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, St
     public UniqueEmailValidator(UserService userService) {
         this.userService = userService;
     }
-
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         return this.userService.findUserByEmail(email).isEmpty();
