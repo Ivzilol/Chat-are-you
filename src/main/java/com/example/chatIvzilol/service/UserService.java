5d18068aa2bf942ef231053fd8256c21a679f83c
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-17 10:21:53 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/service/UserService.java
+++ src/main/java/com/example/chatIvzilol/service/UserService.java
@@ -68,7 +68,6 @@ public class UserService {
             authorityRepository.save(authority);
         }
     }
-
     private User createUserOrAdmin(UserRegistrationDTO userRegistrationDTO, MultipartFile file) throws IOException {
         User user = new User();
         user.setUsername(userRegistrationDTO.getUsername());
