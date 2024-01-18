c0bd9e34cf80af6ec8bbda8c39962b8ca7860d59
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-18 10:33:21 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/service/UserService.java
+++ src/main/java/com/example/chatIvzilol/service/UserService.java
@@ -127,7 +127,6 @@ public class UserService {
     public Optional<User> findByEmail(String email) {
         return this.userRepository.findByEmail(email);
     }
-
     public void sendEmailNewPassword(Optional<User> email) throws MessagingException, UnsupportedEncodingException {
         String subject = "Forgotten password";
         String senderName = "Chat are you Team";
