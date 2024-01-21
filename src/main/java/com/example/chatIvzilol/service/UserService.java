2f6d3ed5463eda6fa29f6b0a1345cd070f9f344f
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-21 10:12:24 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/service/UserService.java
+++ src/main/java/com/example/chatIvzilol/service/UserService.java
@@ -56,6 +56,7 @@ public class UserService {
         this.customPasswordEncoder = customPasswordEncoder;
         this.authenticationManager = authenticationManager;
     }
+
     public Optional<User> findUserByUsername(String username) {
         return this.userRepository.findByUsername(username);
     }
