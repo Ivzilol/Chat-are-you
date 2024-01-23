d48e6560a87a771016bfc6600d03017216e728d0
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-23 16:37:03 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/service/ChatRoomService.java
+++ src/main/java/com/example/chatIvzilol/service/ChatRoomService.java
@@ -70,6 +70,7 @@ public class ChatRoomService {
        }
        return false;
     }
+
     public boolean removeUserFromChatRoom(String roomId, User user) {
         User userForRemove = this.userRepository.findByUserId(user.getId());
         ChatRoom chatRoom = this.chatRoomRepository.findByUniqueCode(roomId);
