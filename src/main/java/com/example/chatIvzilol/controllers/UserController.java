b619409336a1a7ab6be498b76183c3b3728fd782
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-26 18:08:11 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/controllers/UserController.java
+++ src/main/java/com/example/chatIvzilol/controllers/UserController.java
@@ -62,7 +62,7 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
-    
+
     @PostMapping("/register/verify/{verification}")
     private ResponseEntity<?> verificationUser(@PathVariable String verification) {
         User user = this.userService.validateUser(verification);
@@ -72,6 +72,7 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
+
     @PostMapping("/register/forgotten-password")
     public ResponseEntity<?> forgottenPasswordEmail(@RequestBody ForgottenPasswordEmailDto forgottenPasswordDto) throws MessagingException, UnsupportedEncodingException {
         Optional<User> email = this.userService.findByEmail(forgottenPasswordDto.getEmail());
