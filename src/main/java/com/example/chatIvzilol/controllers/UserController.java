5ad0961dc73d39e605e1c3185ae9da939b40bfeb
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-29 10:46:55 +0200

    Refactor

--- src/main/java/com/example/chatIvzilol/controllers/UserController.java
+++ src/main/java/com/example/chatIvzilol/controllers/UserController.java
@@ -62,7 +62,6 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
-
     @PostMapping("/register/verify/{verification}")
     private ResponseEntity<?> verificationUser(@PathVariable String verification) {
         User user = this.userService.validateUser(verification);
