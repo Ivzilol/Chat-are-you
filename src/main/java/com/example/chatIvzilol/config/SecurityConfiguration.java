2fd87ae6041a4c24022cf83758d65d95ad924f12
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-13 18:27:36 +0300

    Refactor

--- src/main/java/com/example/chatIvzilol/config/SecurityConfiguration.java
+++ src/main/java/com/example/chatIvzilol/config/SecurityConfiguration.java
@@ -51,7 +51,6 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
