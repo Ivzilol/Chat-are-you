41dbd680075fed49db8a5d1958930f288c81fcc2
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-13 18:27:44 +0300

    Refactor

--- src/main/java/com/example/chatIvzilol/config/SecurityConfiguration.java
+++ src/main/java/com/example/chatIvzilol/config/SecurityConfiguration.java
@@ -65,6 +65,5 @@ public class SecurityConfiguration {
                 .addFilterBefore(
                         jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-
     }
 }
